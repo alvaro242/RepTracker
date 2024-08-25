fcbc94534f4bb009fd575dc07be81b4f981f584a
Alvaro Mora <alvaro1379@gmail.com>
2024-08-25 20:47:57 +0100

    Revert "reset project"
    This reverts commit 13460b68b60d8d563cf498369339ab79963c8d1c.

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,11 +1,9 @@
-import { Link, type Href } from 'expo-router';
+import { Link } from 'expo-router';
 import { openBrowserAsync } from 'expo-web-browser';
 import { type ComponentProps } from 'react';
 import { Platform } from 'react-native';
 
-
-
-type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: Href };
+type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: string };
 
 export function ExternalLink({ href, ...rest }: Props) {
   return (
@@ -18,7 +16,7 @@ export function ExternalLink({ href, ...rest }: Props) {
           // Prevent the default behavior of linking to the default browser on native.
           event.preventDefault();
           // Open the link in an in-app browser.
-          await openBrowserAsync(href.toString());
+          await openBrowserAsync(href);
         }
       }}
     />
